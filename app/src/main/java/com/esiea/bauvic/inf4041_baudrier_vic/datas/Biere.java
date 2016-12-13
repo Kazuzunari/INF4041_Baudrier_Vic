c9c33a92b1fc99019dfc58f94a6255c789fe04f2
benjaminVic <benjamin.vic.lallart@gmail.com>
2016-12-13 16:33:34 +0100

    Completed Class Biere
    Added attributes
    Added constructor and setter

--- app/src/main/java/com/esiea/bauvic/inf4041_baudrier_vic/datas/Biere.java
+++ app/src/main/java/com/esiea/bauvic/inf4041_baudrier_vic/datas/Biere.java
@@ -5,4 +5,54 @@ package com.esiea.bauvic.inf4041_baudrier_vic.datas;
  */
 
 public class Biere {
+	
+
+	private final String category;
+	private final String country;
+	private final String dateCreation;
+	private final String description;
+	private final String name;
+	private final int note;
+	private final String photoPath;
+	
+	public Biere (String category, String country,
+			String dateCreation, String description, String name,
+			int note, String photoPath){
+		this.category = category;
+		this.country = country;
+		this.dateCreation = dateCreation;
+		this.description = description;
+		this.name = name;
+		this.note = note;
+		this.photoPath = photoPath;
+	}
+	
+	public String getCategory(){
+		return this.category;
+	}
+	
+	public String getCountry(){
+		return this.country;
+	}
+	
+	public String getDateCreation(){
+		return this.dateCreation;
+	}
+	
+	public String getDescription(){
+		return this.description;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public int getNote(){
+		return this.note;
+	}
+	
+	public String getPhotoPath(){
+		return this.getPhotoPath();
+	}
+
 }
