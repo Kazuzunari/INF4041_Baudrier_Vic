20cb5b96a6b55bb0ca2ec5cc4bb46b19b0471d3a
Kazuzunari <baudrier@et.esiea.fr>
2016-12-30 00:02:15 +0100

    sheit

--- app/src/main/java/com/esiea/bauvic/inf4041_baudrier_vic/main/MainActivity.java
+++ app/src/main/java/com/esiea/bauvic/inf4041_baudrier_vic/main/MainActivity.java
@@ -1,53 +1,83 @@
+/*
+* Copyright 2013 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+
 package com.esiea.bauvic.inf4041_baudrier_vic.main;
 
-import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-import android.net.ConnectivityManager;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.esiea.bauvic.inf4041_baudrier_vic.R;
-import com.esiea.bauvic.inf4041_baudrier_vic.db_handling.BiereDAO;
-import com.esiea.bauvic.inf4041_baudrier_vic.db_handling.DBHandler;
+import com.esiea.bauvic.inf4041_baudrier_vic.datas.Biere;
+import com.esiea.bauvic.inf4041_baudrier_vic.datas.BiereAdapter;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
-    private RecyclerView mRecyclerView;
-    private LinearLayoutManager mLinearLayoutManager;
-    private DBHandler dbh;
 
-    public MainActivity(){
-        super();
-        dbh = new DBHandler(MainActivity.this);
-    }
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        //ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        //if (cm.getActiveNetworkInfo().isConnected())
-        //    launch_get_JSON_network();
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview_item);
-        mLinearLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLinearLayoutManager);
-        BiereDAO biereDAO = new BiereDAO();
-        SQLiteDatabase dhb = dbh.getReadableDatabase();
-        System.out.println(dhb);
+        RecyclerView recList = (RecyclerView) findViewById(R.id.cardList);
+        recList.setHasFixedSize(true);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        recList.setLayoutManager(llm);
 
-        int test1 = biereDAO.getIdOfCategory(dhb,"Bavaroise");
-        int test2 = biereDAO.getIdOfCountry(dhb,"Inconnu");
-        System.out.println("Test idCategory : "+test1);
-        System.out.println("Test idCountry : "+test2);
-
+        BiereAdapter b = new BiereAdapter(createList(30));
+        recList.setAdapter(b);
     }
-/*
+
+
     @Override
-    protected void onOpen(SQLiteDatabase sqLiteDatabase){
-        sqLiteDatabase = dbh.getWritableDatabase();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.options, menu);
+        return true;
     }
-    */
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private List<Biere> createList(int size) {
+
+        List<Biere> result = new ArrayList<>();
+        for (int i=1; i <= size; i++) {
+            Biere b = new Biere("cat" + i, "country"+i, "date"+i, "blabla"+i, "bière"+i, i % 6, "photo"+i);
+            result.add(b);
+
+        }
+
+        return result;
+    }
+
+
 }
